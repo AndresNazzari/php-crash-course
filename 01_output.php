dd944cb381582a39aa163bf0b67d62bf5041620b
Andres Nazzari <lordcliff@gmail.com>
2023-02-26 09:08:03 -0300

    output types (echo, print, var_dump)

---
+++ 01_output.php
@@ -0,0 +1,33 @@
+<?php
+//echo - Output strings, numbers, html, etc
+// echo "Hello ";
+// echo 123;
+// echo [2,3,4,6]; esto da error
+
+//print - Wprks like echo, but can only take in a single argunment
+// print "Hello";
+
+//print_r - prints single values and arrays
+// print_r([2,3,4,6]);
+
+//var_dump - returns more info like data type and lenght
+// var_dump("Hello")
+
+//var_export - similar to var dump. Outputs a string representation of a variable
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+   echo "Hello"; 
+?>
+
+</body>
+</html>
